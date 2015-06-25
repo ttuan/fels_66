a34a647a68a93e736d4f20c74e86caf2e3271311
ttuan <tuantv.nhnd@gmail.com>
2015-06-25 16:59:20 +0700

    Finish log in and log out feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+  validates :name, presence: true
+  validates :password, presence: true
+
+  has_secure_password
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
@@ -10,4 +15,28 @@ class User < ActiveRecord::Base
 
   has_many :activities, dependent: :destroy
   has_many :lessons, dependent: :destroy
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create string, cost: cost
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
 end
