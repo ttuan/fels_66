fcf184aeac870ed3777c6e835c7076b24acb142d
ttuan <tuantv.nhnd@gmail.com>
2015-07-01 16:46:02 +0700

    Showing categories for normal users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
+
 Rails.application.routes.draw do
   root                     "static_pages#home"
-  get     "help"      =>   "static_pages#help"
-  get     "about"     =>   "static_pages#about"
-  get     "contact"   =>   "static_pages#contact"
-  get     "signup"    =>   "users#new"
-  get     "login"     =>   "sessions#new"
-  post    "login"     =>   "sessions#create"
-  delete  "logout"    =>   "sessions#destroy"
+  get     "help"       =>   "static_pages#help"
+  get     "about"      =>   "static_pages#about"
+  get     "contact"    =>   "static_pages#contact"
+  get     "signup"     =>   "users#new"
+  get     "login"      =>   "sessions#new"
+  post    "login"      =>   "sessions#create"
+  delete  "logout"     =>   "sessions#destroy"
+  get     "categories" =>   "categories#index"
+
   resources :users
   resources :relationships, only: [:create, :destroy]
   namespace :admin do
